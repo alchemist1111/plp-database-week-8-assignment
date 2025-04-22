4601617f3593ab35642b64cbdf3afdb0c671f332
alchemist1111 <emoyoeugine@gmail.com>
2025-04-22 19:57:18 +0300

    question 2 update

--- question2/db.py
+++ question2/db.py
@@ -1,13 +1,18 @@
 import mysql.connector
 from mysql.connector import Error
+import os
+from dotenv import load_dotenv
+
+# Load environment variables from .env file
+load_dotenv()
 
 def get_db_connection():
     try:
         connection = mysql.connector.connect(
-            host='localhost',
-            database='contact_bookdb',
-            user='root',
-            password='SuntonC3@25'
+            host=os.getenv("DB_HOST"),
+            database=os.getenv("DB_NAME"),
+            user=os.getenv("DB_USER"),
+            password=os.getenv("DB_PASSWORD")
         )
         if connection.is_connected():
             return connection
